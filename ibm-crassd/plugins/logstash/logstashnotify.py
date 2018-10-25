3c127741a1ec58eba2be3331df8db40e595f135e
Justin Thaler <thalerj@us.ibm.com>
2018-10-25 15:53:53 -0500

    Update logstash plugin for reported messages
    Provides an update to the logstash plugin so the json data structure is parsed by the ELK stack better. Also ensures compatibility with python 3.
    
    Signed-off-by: Justin Thaler thalerj@us.ibm.com

--- ibm-crassd/plugins/logstash/logstashnotify.py
+++ ibm-crassd/plugins/logstash/logstashnotify.py
@@ -6,7 +6,7 @@ try:
     import Queue as queue
 except ImportError:
     import queue
-
+import traceback
 import socket
 
 def connectToSocket(logSocket, host, port, logstashDown):
@@ -20,27 +20,53 @@ def connectToSocket(logSocket, host, port, logstashDown):
         @return: Connection status after attempting to connect. True when successful. 
     """
     connected = False
-    # Try 3 times to open the socket connection
-    for x in range(0, 3): 
-        try:
-            logSocket.connect((host,port))
+    errorString = ""
+    
+    #check if socket is active by receiving and checking for eof
+    try:
+        logSocket.settimeout(0.1)
+        data = logSocket.recv(4096)
+        if not data:
+            connected = False
+        else:
             connected = True
-            break
-        except socket.error as erString:
-            continue
+    except socket.timeout:
+        connected = True
+    except Exception as e:
+        connected = False
+        errorString = e
+    
+        
+    # Try 3 times to open the socket connection
+    if not connected:
+        for x in range(0, 3): 
+            try:
+                logSocket.connect((host,port))
+                connected = True
+                break
+            except socket.error as erString:
+                errorString = erString
+                continue
     if not connected:
         if not logstashDown:
-            config.errorLogger(syslog.LOG_ERR, "Logstash connection failure: {}".format(erString))
+            config.errorLogger(syslog.LOG_ERR, "Logstash connection failure: {}".format(errorString))
     return connected
 
 def writeToSocket(logSocket, alert2Send):
     #while not config.killNow:
         sendFailed = False
         #alert2Send = logqueue.get()
+        #data2send = json.dumps(alert2Send['logEntry'],sort_keys=False, indent=4, separators=(',', ': ')).encode()
+#         eventTime =datetime.datetime.fromtimestamp(int(alert2Send['logEntry']['timestamp'])).strftime("%Y-%m-%d %H:%M:%S")
+        data2send = json.dumps(alert2Send['logEntry'], indent=0, separators=(',', ':')).replace('\n','') +"\n"
+        data2send = data2send.encode()
         try:
-            logSocket.sendall(json.dumps(alert2Send['logEntry'],sort_keys=False, indent=4, separators=(',', ': ')))
+            logSocket.sendall(data2send)
         except socket.error:
             sendFailed = True
+        except Exception as e:
+            traceback.print_tb(e.__traceback__)
+            print(e)
         if sendFailed:
             with config.lock:
                 host = config.pluginConfigs['logstash']['host']
@@ -49,7 +75,7 @@ def writeToSocket(logSocket, alert2Send):
             if connectToSocket(logSocket, host, port, alert2Send['entityAttr']['logstash']['receiveEntityDown']):
                 sendFailed = False
                 try:
-                    logSocket.sendall(json.dumps(alert2Send['logEntry'],sort_keys=False, indent=4, separators=(',', ': ')))
+                    logSocket.sendall(data2send)
                     sendFailed = False
                 except socket.error:
                     sendFailed = True
@@ -61,6 +87,8 @@ def writeToSocket(logSocket, alert2Send):
                 with config.lock:
                     alert2Send['entityAttr']['logstash']['failedFirstTry']= False
                     alert2Send['entityAttr']['logstash']['receiveEntityDown']= True
+        if not sendFailed:
+            config.errorLogger(syslog.LOG_INFO, "Sent to logstash: {analert}".format(analert = data2send))
         return not sendFailed
     #logSocket.close()
 
@@ -102,7 +130,6 @@ def notifyLogstash(cerEvent, impactedNode, entityAttr):
     queDict = {}
     queDict['entityAttr'] = entityAttr
     queDict['logEntry'] = newAlert
-#     config.pluginVars['logstash']['logstashQueue'].put(queDict)
     return writeToSocket(config.pluginVars['logstash']['logstashSocket'], queDict)
     
      
